93256c1cca4428327453762a14908ad15536bd67
VicPat25 <vmpd.25@gmail.com>
2022-11-05 21:10:38 -0500

    Ajustes device

--- app/api/routes/devices.js
+++ app/api/routes/devices.js
@@ -3,31 +3,146 @@ const router = express.Router();
 
 const { checkAuth } = require('../middlewares/authentication.js')
 
+/*
+  Models
+*/
 
-router.get("/device", checkAuth ,(req, res) => {
+import Device from '../models/device.js';
 
-  console.log(req.userData);
+/*
+  API
+*/
+
+//GET DEVICES
+router.get("/device", checkAuth, async (req, res) => {
+
+  try {
+    const userId = req.userData._id;
+    const devices = await Device.find({ userId: userId });
+      
+    const toSend = {
+      status: "success",
+      data: devices
+    }
   
-  req.userData.userId
+    res.json(toSend);
 
-  const toSend = {
-    status: "success",
-    data: "[2 , 3 , 4 , 5 ]"
-  };
+  } catch (error) {
+    console.log("ERROR CREATING NEW DEVICE");
+  
+    const toSend = {
+      status: "error",
+      error: error
+    }
+  
+    return res.status(500).json(toSend);
 
-  return res.status(200).json(toSend);
-
-});
-
-router.post("/device", (req, res) => {
-
-});
-
-router.delete("/device", (req, res) => {
+  }
 
 });
 
-router.put("/device", (req, res) => {
+//NEW DEVICE
+router.post("/device", checkAuth, async (req, res) => {
+  try {
+    const userId = req.userData._id;
+    var newDevice = req.body.newDevice;
+
+    newDevice.userId = userId;
+    newDevice.createdTime = Date.now();
+
+    const device = await Device.create(newDevice);
+
+    const toSend = {
+      status: "success"
+    };
+
+    return res.json(toSend);
+  } catch (error) {
+    console.log("ERROR CREATING NEW DEVICE");
+    console.log(error);
+
+    const toSend = {
+      status: "error",
+      error: error
+    };
+
+    return res.status(500).json(toSend);
+  }
 });
+
+//DELETE DEVICE
+router.delete("/device", checkAuth, async(req, res) => {
+
+try {
+    const userId = req.userData._id;
+    const dId = req.query.dId;
+
+    const result = await Device.deleteOne({userId: userId, dId: dId});
+  
+    const toSend = {
+      status: "success",
+      data: result
+    };
+  
+    return res.json(toSend);
+    
+  } catch (error) {
+
+    console.log("ERROR DELETING DEVICE");
+    console.log(error);
+
+    const toSend = {
+      status: "error",
+      error: error
+    };
+
+    return res.status(500).json(toSend);
+  }
+
+});
+
+//UPDATE DEVICE
+router.put("/device", checkAuth, (req, res) => {
+  const dId = req.body.dId;
+  const userId = req.userData._id;
+
+  if (selectDevice(userId, dId)) {
+    const toSend = {
+      status: "success"
+    };
+
+    return res.json(toSend);
+  } else {
+    const toSend = {
+      status: "error"
+    };
+
+    return res.json(toSend);
+  }
+});
+
+/*
+  functions
+*/
+async function selectDevice(userId, dId) {
+  try {
+    const result = await Device.updateMany(
+      { userId: userId },
+      { selected: false }
+    );
+
+    const result2 = await Device.updateOne(
+      { dId: dId, userId: userId },
+      { selected: true }
+    );
+
+    return true;
+
+  } catch (error) {
+    console.log("ERROR IN 'selectDevice' FUNCTION ");
+    console.log(error);
+    return false;
+  }
+}
 
 module.exports = router;
